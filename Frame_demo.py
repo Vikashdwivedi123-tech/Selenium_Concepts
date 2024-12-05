e138c2e514990baaf6166d1061a29ffc201f41f2
Vikash Dwivedi <139767359+Vikashdwivedi123-tech@users.noreply.github.com>
2024-12-05 22:23:01 +0530

    Update Frame_demo.py

--- Frame_demo.py
+++ Frame_demo.py
@@ -5,7 +5,7 @@ from selenium.webdriver import ActionChains
 driver = webdriver.Chrome()
 
 driver.get("https://the-internet.herokuapp.com/iframe")
-driver.switch_to.frame("mce_0_ifr")  # 'mce_0_ifr is the frame id.
+driver.switch_to.frame("mce_0_ifr")  # 'mce_0_ifr is the frame id, or you can write the frame name as well. 
 driver.find_element(By.ID, "tinymce").clear()  # It clear the all text present at the location.
 driver.find_element(By.ID, "tinymce").send_keys("I am able to automate frame")
 
